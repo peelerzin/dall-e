a93b1fc9ca7b0c37f318a1d74f92ee0be76fa637
peeler <t.delourme11@gmail.com>
2023-06-16 20:10:17 +0200

    vercel ready

--- client /src/pages/CreatePost.jsx
+++ client /src/pages/CreatePost.jsx
@@ -31,15 +31,18 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        });
+        const response = await fetch(
+          "https://dall-e-vkng.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: form.prompt,
+            }),
+          }
+        );
 
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
@@ -62,7 +65,7 @@ const CreatePost = () => {
       setLoading(true); // if yes setloading to true
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post", //call post image route
+          "https://dall-e-vkng.onrender.com/api/v1/post", //call post image route
           {
             method: "POST",
             headers: {
